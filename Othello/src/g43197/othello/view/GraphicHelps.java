45bc88ea82048d25e45c8e344fe07cad43405d3a
Philippe <Philippe@192.168.1.23>
2017-12-08 20:55:46 +0100

    Possible final commit Progress indicator

--- Othello/src/g43197/othello/view/GraphicHelps.java
+++ Othello/src/g43197/othello/view/GraphicHelps.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.ProgressIndicator;
 
 /**
  * Class with multiple Graphic helps like a progressbar.
@@ -18,15 +19,29 @@ class GraphicHelps extends VBox {
     //TODO ajouter autre progresindicator
     private final Facade game;
     private final ProgressBar pieceProgress;
+    private final ProgressIndicator gameProgress;
+    private final double nbTiles;
 
     GraphicHelps(Facade game) {
         this.game = game;
-        HBox pieceProgressContainer = new HBox(10);
+        final HBox pieceProgressContainer = new HBox(10);
         pieceProgress = new ProgressBar();
         pieceProgress.setStyle("-fx-accent: black;");
         pieceProgressContainer.getChildren().addAll(new Label("Black/White"), pieceProgress);
         updatePieceProgress();
-        this.getChildren().addAll(pieceProgressContainer);
+
+        final HBox gameProgressContainer = new HBox(10);
+        gameProgress = new ProgressIndicator(0);
+        nbTiles = Math.pow(game.getMaxRowsCols(), 2);
+        gameProgressContainer.getChildren().addAll(new Label("Game progress"), gameProgress);
+        updateGameProgress();
+
+        this.getChildren().addAll(pieceProgressContainer, gameProgressContainer);
+    }
+
+    void update() {
+        updatePieceProgress();
+        updateGameProgress();
     }
 
     private void updatePieceProgress() {
@@ -41,8 +56,11 @@ class GraphicHelps extends VBox {
         pieceProgress.setProgress(nbBlackPieces / (nbBlackPieces + otherPieces));
     }
 
-    void update() {
-        updatePieceProgress();
+    private void updateGameProgress() {
+        int nbPiecesOnBoard = game.getPlayers().stream()
+                .map(player -> player.getScore())
+                .reduce(0, Integer::sum);
+        gameProgress.setProgress(nbPiecesOnBoard / nbTiles);
     }
 
 }
